72aa5827aa9b5fd02b793a587483a40429026348
Gordon Marler <gmarler@gmarler.com>
2015-05-27 11:31:41 -0400

    Updates to perl module to support XS

--- lib/Bzip2/RandomAccess.pm
+++ lib/Bzip2/RandomAccess.pm
@@ -1,10 +1,21 @@
-use strict;
-use warnings;
-
 package Bzip2::RandomAccess;
 
+use strict;
+use warnings;
+use XSLoader;
+
 # VERSION
+our $VERSION = '0.001';
 
 # ABSTRACT: Allows random access to a bz2 compressed file
 
+XSLoader::load('Bzip2::RandomAccess', $VERSION);
+
 1;
+
+__END__
+
+=head1 NAME
+
+Bzip2::RandomAccess - Interface to a library to allows random access to bz2
+compressed archive files
